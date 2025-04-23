1418725481871903934ec84f42941f3aba756e62
hyunwestpark <rainday0828@gmail.com>
2025-04-23 12:34:29 +0900

    feat: 주문 엔티티 작성

---
+++ backend/src/main/java/io/pentacore/backend/product/domain/Order.java
@@ -0,0 +1,34 @@
+package io.pentacore.backend.product.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @Column(name = "order_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Column(length = 40, nullable = false)
+    private String email;
+
+    @Column(length = 50, nullable = false)
+    private String address;
+
+    @Column(name = "postal_code", length = 5, nullable = false)
+    private String postalCode;
+
+    @Column(name = "ordered_at", nullable = false)
+    private LocalDateTime orderedAt;
+
+    @Column(name = "total_price", nullable = false, columnDefinition = "INT UNSIGNED")
+    private Integer totalPrice;
+
+//    상품 목록 생기면 작성
+//    @OneToMany(mappedBy = "order")
+//    private List<OrderProduct> orderProducts = new ArrayList<>();
+}
