c4776513d26e2ce62af93a725ae83b347c0456f0
hs986 <94986211+hs986@users.noreply.github.com>
2025-04-23 12:40:56 +0900

    feat : 상품 리포지토리 생성 fix : Products에서 Product 로 수정

--- backend/src/main/java/io/pentacore/backend/product/domain/Product.java
+++ backend/src/main/java/io/pentacore/backend/product/domain/Product.java
@@ -1,5 +1,6 @@
 package io.pentacore.backend.product.domain;
 
+import io.pentacore.backend.admin.domain.Admin;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Products {
+public class Product {
 
 
     @Id
