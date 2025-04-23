f63865539d7978807dda04ff6b34151886147131
hyunwestpark <rainday0828@gmail.com>
2025-04-23 11:39:50 +0900

    feat: 주문 엔티티 작성

--- backend/src/main/java/io/pentacore/backend/product/domain/Order.java
+++ backend/src/main/java/io/pentacore/backend/product/domain/Order.java
@@ -3,14 +3,13 @@ package io.pentacore.backend.product.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 @Entity
 @Table(name = "orders")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
 
     @Column(length = 40, nullable = false)
@@ -28,7 +27,4 @@ public class Order {
     @Column(name = "total_price", nullable = false, columnDefinition = "INT UNSIGNED")
     private Integer totalPrice;
 
-    // OrderProduct는 나중에 추가
-//    @OneToMany(mappedBy = "order")
-//    private List<OrderProduct> orderProductList = new ArrayList<>();
 }
