4153d76d72c7e82f8361580579d912faeee2e6b4
hs986 <94986211+hs986@users.noreply.github.com>
2025-04-23 12:48:14 +0900

    feat : 상품 리포지토리 생성 fix : Products에서 Product 로 수정

--- backend/src/main/java/io/pentacore/backend/product/domain/Product.java
+++ backend/src/main/java/io/pentacore/backend/product/domain/Product.java
@@ -11,7 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
 
-
     @Id
     @Column(name ="product_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
