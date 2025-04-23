73d0ba14a7a28e4e8d0898dc58d498e43649aeaa
silver-eunjoo <leeeun5423@naver.com>
2025-04-23 10:41:45 +0900

    feat : admin 엔티티 클래스 작성

---
+++ backend/src/main/java/io/pentacore/backend/admin/domain/Admin.java
@@ -0,0 +1,28 @@
+package io.pentacore.backend.admin.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+public class Admin {
+
+    @Id
+    @Column(name="admin_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length=40, nullable=false)
+    private String email;
+
+    @Column(length=40, nullable=false)
+    private String password;
+
+}
