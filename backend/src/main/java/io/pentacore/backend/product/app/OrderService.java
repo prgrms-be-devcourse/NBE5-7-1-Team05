64bd6de56575675c613b29c2c2ac16453bb089ca
silver-eunjoo <leeeun5423@naver.com>
2025-04-24 11:28:17 +0900

    feat : 주문이 존재하지 않을 때 예외 처리 & Transactional 추가

--- backend/src/main/java/io/pentacore/backend/product/app/OrderService.java
+++ backend/src/main/java/io/pentacore/backend/product/app/OrderService.java
@@ -2,8 +2,10 @@ package io.pentacore.backend.product.app;
 
 import io.pentacore.backend.product.dao.OrderProductRepository;
 import io.pentacore.backend.product.dao.OrderRepository;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +13,11 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
+    @Transactional
     public void deleteOrder(Long orderId) {
+        orderRepository.findById(orderId)
+                        .orElseThrow(() -> new NoSuchElementException("주문이 존재하지 않습니다."));
+
         orderRepository.deleteById(orderId);
     }
 }
