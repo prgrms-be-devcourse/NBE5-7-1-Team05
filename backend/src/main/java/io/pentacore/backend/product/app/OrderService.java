727b096842122fa102984eeefd9e5e64c01cb68c
Hyunseo Sean Park <123967536+hyunwestpark@users.noreply.github.com>
2025-04-24 12:01:38 +0900

    feat: 주문 조회 기능 추가

--- backend/src/main/java/io/pentacore/backend/product/app/OrderService.java
+++ backend/src/main/java/io/pentacore/backend/product/app/OrderService.java
@@ -1,8 +1,11 @@
 package io.pentacore.backend.product.app;
 
-import io.pentacore.backend.product.dao.OrderProductRepository;
-import io.pentacore.backend.product.dao.OrderRepository;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import io.pentacore.backend.product.dao.OrderRepository;
+import io.pentacore.backend.product.domain.Order;
+import io.pentacore.backend.product.dto.OrderResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,8 +19,20 @@ public class OrderService {
     @Transactional
     public void deleteOrder(Long orderId) {
         orderRepository.findById(orderId)
-                        .orElseThrow(() -> new NoSuchElementException("주문이 존재하지 않습니다."));
+                .orElseThrow(() -> new NoSuchElementException("주문이 존재하지 않습니다."));
 
         orderRepository.deleteById(orderId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<OrderResponseDto> getOrdersByEmail(String email) {
+        List<Order> orders = orderRepository.findByEmailWithOrderProducts(email);
+
+        List<OrderResponseDto> orderResponseDtos = new ArrayList<>();
+        for (Order order : orders) {
+            orderResponseDtos.add(OrderResponseDto.from(order));
+        }
+
+        return orderResponseDtos;
     }
 }
