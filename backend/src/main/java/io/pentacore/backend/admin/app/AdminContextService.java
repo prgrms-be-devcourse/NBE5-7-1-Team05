5d693c207b824270331725be3670a09f12d66a66
silver-eunjoo <leeeun5423@naver.com>
2025-04-26 17:48:15 +0900

    feat : 예외 조건문 수정

--- backend/src/main/java/io/pentacore/backend/admin/app/AdminContextService.java
+++ backend/src/main/java/io/pentacore/backend/admin/app/AdminContextService.java
@@ -3,6 +3,8 @@ package io.pentacore.backend.admin.app;
 import io.pentacore.backend.admin.domain.Admin;
 import io.pentacore.backend.admin.dto.AdminDetails;
 import io.pentacore.backend.global.unit.BaseResponse;
+import io.pentacore.backend.global.unit.exception.CustomException;
+import io.pentacore.backend.global.unit.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
@@ -16,9 +18,8 @@ public class AdminContextService {
 
     public Long getCurrentAdminId() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null || (auth.getPrincipal() instanceof AdminDetails)) {
-            BaseResponse.error("인증된 관리자가 아닙니다", HttpStatus.UNAUTHORIZED);
-
+        if (auth == null || !(auth.getPrincipal() instanceof AdminDetails)) {
+            throw new CustomException(ErrorCode.UNAUTHORIZED);
         }
         AdminDetails adminDetails = (AdminDetails) auth.getPrincipal();
         return adminDetails.getId();
